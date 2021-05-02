beaee10f0b8db5c0309a50508580b5c69bcb916d
Loïc Piazzalunga <73880144+lpiazza08@users.noreply.github.com>
2021-05-02 09:50:17 +0200

    Update log_in.js

--- imports/ui/log_in/log_in.js
+++ imports/ui/log_in/log_in.js
@@ -1,5 +1,27 @@
 if (Meteor.isClient) {
     Template.register.events({
-        'submit form': function(event, template)
-    })
+        'submit form': function(event, template){
+            event.preventDefault();
+            var emailVar = template.find('#email').value;
+            var passwordVar = template.find('#password').value;
+            Accounts.createUser({
+            email: emailVar,
+            password: passwordVar
+        });
+    }
+});
+Template. login.events({
+    'submit form': function(event, template){
+        event.preventDefault();
+        var emailVar = template.find('#login-email').value;
+        var passwordVar = template.find('#login-password').value;
+        meteor.loginWithPassword(emailVar,passwordVar);
+    }
+});
+Template.dashboard.events({
+    'click.logout': function(event){
+        evewnt.preventDefault();
+        Meteor.logout();
+    }
+});
 }
