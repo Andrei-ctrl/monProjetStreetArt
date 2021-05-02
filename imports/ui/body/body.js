6282fa84541d927067b6eaf89704d9a99c1dfba9
Adrien0114 <adrien.sarrasin@unil.ch>
2021-05-02 10:59:58 +0200

    Mise en place de la Map avec Loïc

--- imports/ui/body/body.js
+++ imports/ui/body/body.js
@@ -2,5 +2,6 @@ import './body.html';
 import '../collection/collection.js';
 import '../element_collection/element_collection.js';
 import '../log_in/log_in.js';
+import '../api/maps/maps-geoloc.js'
 
 
