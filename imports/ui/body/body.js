fae7fdd79b2137770dd8f35c5439db6cd412aa0e
Adrien0114 <adrien.sarrasin@unil.ch>
2021-05-11 00:06:22 +0200

    Mise en place des routes
    Les premiers pas

--- imports/ui/body/body.js
+++ imports/ui/body/body.js
@@ -1,5 +1,6 @@
 import './body.html';
+import '../creer_un_compte/creercompte.js';
 import '../collection/collections.js';
 import '../element_collection/element_collection.js';
-import '../creer_un_compte/creercompte.js';
 import '../../api/maps/maps-geoloc.js';
+import '../../../lib/routing.js';
