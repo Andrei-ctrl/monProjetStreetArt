8664d6dd6394ddfabffb1e45dc70a313ca1063a9
Loïc Piazzalunga <73880144+lpiazza08@users.noreply.github.com>
2021-05-11 19:13:34 +0200

    login avec Adri

--- imports/ui/body/body.js
+++ imports/ui/body/body.js
@@ -7,3 +7,25 @@ import '../accueil/accueil.js';
 import '../creercompte/creercompte.js';
 import '../connexion/connexion.js';
 import '../accueilLog/accueilLog.js';
+
+import { Template } from 'meteor/templating';
+import { ReactiveVar } from 'meteor/reactive-var';
+import { Accounts } from 'meteor/accounts-base';
+import { meteor } from 'meteor/meteor';
+
+Template.connexion.events({
+
+    'click #connexion-btn' (event) {
+        event.preventDefault();
+        let mail = document.getElementById('email').value;
+        let mdp = document.getElementById('password').value;
+        Meteor.loginWithPassword(mail, mdp, (error) => {
+            if (error){
+                alert(error.message)
+            } else{
+                SetTimeout(() => FlowrouterGo('apropos'), 200); 
+            }
+        });
+    }
+
+});
--- imports/ui/creercompte/creercompte.js
+++ imports/ui/creercompte/creercompte.js
@@ -3,7 +3,8 @@ import './creercompte.html';
 import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 import { Accounts } from 'meteor/accounts-base';
-import { meteor } from 'meteor/meteor';
+import { Meteor } from 'meteor/meteor';
+import { FlowRouter } from 'meteor/ostrio:flow-router-extra';
 
 Template.creercompte.events({
     
