89f237d83bee03b98ecd51eadc4bbbcaab4bd5a2
Adrien0114 <adrien.sarrasin@unil.ch>
2021-05-11 18:09:29 +0200

    Update connexion.js

--- imports/ui/connexion/connexion.js
+++ imports/ui/connexion/connexion.js
@@ -10,9 +10,19 @@ Template.connexion.events({
   Template.connexion.events({
     'click #connexion-btn' (event) {
       event.preventDefault();
-      FlowRouter.go('accueilLog');
+      let mail = document.getElementById('email').value;
+      let mdp = document.getElementById('password').value;
+      Meteor.loginWithPassword(mail, mdp, (error) => {
+        if (error){
+            alert(error.message)
+        } else{
+            SetTimeout(() => FlowrouterGo('accueilLog'), 200); 
+        }
+      });
     }
   });
+
+
 
   Template.connexion.events({
     'click #creercompte' (event) {
