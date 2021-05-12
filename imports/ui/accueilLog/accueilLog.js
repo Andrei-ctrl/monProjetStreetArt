481a9af3f9afe34d01b5efe8da062089854ac2ba
Loïc Piazzalunga <73880144+lpiazza08@users.noreply.github.com>
2021-05-12 09:28:38 +0200

    logout avec Adri

--- imports/ui/accueilLog/accueilLog.js
+++ imports/ui/accueilLog/accueilLog.js
@@ -3,3 +3,16 @@ import './accueilLog.css';
 import '../../api/maps/maps-geoloc.js';
 
 
+import { Template } from 'meteor/templating';
+import { Meteor } from 'meteor/meteor';
+
+Template.accueilLog.helpers({
+    utilisateur: () => Meteor.user().username,
+})
+
+Template.accueilLog.events({
+    'click #logout'(event){
+    event.preventDefault();
+    FlowRouter.go('accueil');
+    }
+})
