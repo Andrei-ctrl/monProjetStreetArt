25c8165209148054887eb2d6d8edd1847e8694b9
Adrien0114 <adrien.sarrasin@unil.ch>
2021-05-14 13:26:04 +0200

    Revert "Merge branch 'main' of https://github.com/Adrien0114/monProjetStreetArt into main"
    This reverts commit 0bc7db10c26e9c156ba061a6ff4db0cc603b1c02, reversing
    changes made to 6a26dd8dd2175e88b38d4328f57e3b9113e2f23c.

--- imports/api/maps/maps-geoloc.js
+++ imports/api/maps/maps-geoloc.js
@@ -1,10 +1,9 @@
 import './maps-geoloc.html';
 import './maps-geoloc.css';
-import { ObjetsCollection } from '../../api/collection_DB';
 
 if (Meteor.isClient) {
     var MAP_ZOOM = 15;
-  
+    
     Meteor.startup(function() {
       GoogleMaps.load();
     });
@@ -36,45 +35,7 @@ if (Meteor.isClient) {
           // Center and zoom the map view onto the current position.
           map.instance.setCenter(marker.getPosition());
           map.instance.setZoom(MAP_ZOOM);
-
-          /*
-          var markerTest = new google.maps.Marker({
-            position: new google.maps.LatLng(latLng.lat + 5, latLng.lng + 5),
-            map: map.instance
-          });
-          map.instance.setCenter(markerTest.getPosition());
-          //alert(latLng.lat + " et " + latLng.lng);
-          */
-
-          const contentString =
-          '<div id="content">' +
-          "Bonjour test" +
-          "</div>";
-
-          const infowindow = new google.maps.InfoWindow({
-            content: contentString,
-          });
-
-          marker.addListener("click", () => {
-            infowindow.open(map, marker);
-          });
-        
-
         });
-
-        //Add markers from DB
-        //get markers
-        //"fetch()" retourne la liste des objets de la db
-        var listOeuvres = ObjetsCollection.find({}).fetch();
-        //alert(listOeuvres.length);
-        listOeuvres.forEach(oeuvre => {
-          marker = new google.maps.Marker({
-            position: new google.maps.LatLng(oeuvre.lat, oeuvre.lng),
-            map: map.instance,
-            
-          });
-        });
-
       });
     });
   
@@ -95,3 +56,4 @@ if (Meteor.isClient) {
       }
     });
   }
+  
--- imports/ui/ajouterOeuvre/ajouterOeuvre.js
+++ imports/ui/ajouterOeuvre/ajouterOeuvre.js
@@ -16,11 +16,14 @@ Template.ajouterOeuvre.helpers({
 
 Template.ajouterOeuvre.events({
     'click #ajouterOeuvrePActuelle': function() {
-        //Session.set('confirmationModal', true); // Show modal
-
+        var latLng = Geolocation.latLng();
+        let lat = latLng.lat;
+        let lng = latLng.lng;
+        let image;
+        alert("lat : " + lat + " lng : " + lng);
         //const nouvelObjet = prompt('Ajouter une image !');
         // Appel de la méthode
-        //Meteor.call('ajouterOeuvre', lat, lng, image);
+        Meteor.call('ajouterOeuvre', lat, lng, image);
     },
     'click #ajouterOeuvreNewPos': function() {
         let lat;
@@ -29,17 +32,7 @@ Template.ajouterOeuvre.events({
         const nouvelObjet = prompt('Entrez un nouvel élément !');
         // Appel de la méthode
         Meteor.call('ajouterOeuvre', lat, lng, image);
-    },
-    'click #ok': function() {
-        let lat;
-        let lng;
-        var latLng = Geolocation.latLng();
-        const image = prompt('Entrez un nouvel élément !');
-        // Appel de la méthode
-        Meteor.call('ajouterOeuvre', lat, lng, image);
-        //Session.set('confirmationModal', false); // Show modal
-    },
-    'click #annuler': function() {
-        //Session.set('confirmationModal', false); // Show modal
     }
+
+
 });
