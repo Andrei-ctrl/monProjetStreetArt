74e3fd53ac48c5561a945281771851bc9480aaf0
Adrien0114 <adrien.sarrasin@unil.ch>
2021-05-19 10:14:25 +0200

    Ajouter une image et la position suite
    Co-Authored-By: Loïc Piazzalunga <73880144+lpiazza08@users.noreply.github.com>

--- imports/ui/ajouterOeuvre/ajouterOeuvre.js
+++ imports/ui/ajouterOeuvre/ajouterOeuvre.js
@@ -33,7 +33,36 @@ Template.ajouterOeuvre.events({
             Swal.fire({
               title: 'Vous avez sélectionné cette image',
               imageUrl: e.target.result,
-              imageAlt: 'The uploaded picture'
+              imageAlt: 'The uploaded picture',
+              confirmButtonText: 'suivant',
+            }).then((result) => {
+              if (result.isConfirmed) {
+                Swal.fire({
+                  title: 'Confirmation',
+                  text: "Voulez-vous vraiment ajouter votre position actuelle",
+                  icon: 'warning',
+                  showCancelButton: true,
+                  confirmButtonColor: '#3085d6',
+                  cancelButtonColor: '#d33',
+                  confirmButtonText: 'Oui'
+                }).then((result) => {
+                  if (result.isConfirmed) {
+                      var latLng = Geolocation.latLng();
+                      let lat = latLng.lat;
+                      let lng = latLng.lng;
+                      let image = e.target.result;
+                      // Appel de la méthode
+                      Meteor.call('ajouterOeuvre', lat, lng, image);
+              
+                      //display confirmation message
+                      Swal.fire(
+                          'Ajouté',
+                          'Votre position actuelle (' + lat + ', ' + lng + ') a été ajoutée.',
+                          'success'
+                      )
+                  }
+                })
+              }
             })
           }
           reader.readAsDataURL(file)
@@ -41,31 +70,7 @@ Template.ajouterOeuvre.events({
         
         })()
         
-        /*Swal.fire({
-            title: 'Confirmation',
-            text: "Voulez-vous vraiment ajouter votre position actuelle",
-            icon: 'warning',
-            showCancelButton: true,
-            confirmButtonColor: '#3085d6',
-            cancelButtonColor: '#d33',
-            confirmButtonText: 'Oui'
-          }).then((result) => {
-            if (result.isConfirmed) {
-                var latLng = Geolocation.latLng();
-                let lat = latLng.lat;
-                let lng = latLng.lng;
-                let image = prompt('Ajouter une image !');
-                // Appel de la méthode
-                Meteor.call('ajouterOeuvre', lat, lng, image);
-        
-                //display confirmation message
-                Swal.fire(
-                    'Ajouté',
-                    'Votre position actuelle (' + lat + ', ' + lng + ') a été ajoutée.',
-                    'success'
-                )
-            }
-          })*/
+        /**/
     },
     'click #ajouterOeuvreNewPos': function() {
         let lat;
