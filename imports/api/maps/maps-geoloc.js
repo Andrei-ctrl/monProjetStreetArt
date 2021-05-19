7a2a3c677ad9d38bcea7970568084e52cd0351f2
Adrien0114 <adrien.sarrasin@unil.ch>
2021-05-19 11:20:19 +0200

    Update maps-geoloc.js

--- imports/api/maps/maps-geoloc.js
+++ imports/api/maps/maps-geoloc.js
@@ -73,6 +73,8 @@ if (Meteor.isClient) {
     console.log(listOeuvres);
     listOeuvres.forEach(oeuvre => {
       marker = new google.maps.Marker({
+        // Je distingue le marqueur de la position actuelle de la position des oeuvres
+        icon : 'http://maps.google.com/mapfiles/marker_green.png', 
         position: new google.maps.LatLng(oeuvre.lat, oeuvre.lng),
         map: map.instance,
       });
